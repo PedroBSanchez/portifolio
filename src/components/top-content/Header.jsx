a4c831f4a5360f704f87c51b77b5367b5065bfcf
PedroBSanchez <pedro_b47@hotmail.com>
2022-05-25 14:20:58 -0300

    Criação do header

---
+++ src/components/top-content/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-container">
+        <div className="text-container">
+          <h1 className="text">Welcome to my portifolio</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
